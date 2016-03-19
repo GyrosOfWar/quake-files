a7b0e96bc5c9ebba8586fe45ed3e121994105983
Martin Tomasi <martin.tomasi@gmail.com>
2016-03-19 14:12:47 +0100

    added logging feature

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,10 @@
 extern crate image;
 extern crate byteorder;
 extern crate walkdir;
+#[macro_use]
+extern crate log;
+#[cfg(feature="logging")]
+extern crate env_logger;
 
 pub mod pak;
 pub mod lmp;
@@ -24,28 +28,27 @@ fn main() {
         "create" => {
             let out_file = &args[2];
             let folder = &args[3];
-            println!("Creating PAK file {} from folder {}", out_file, folder);
             let pak = create_pak(folder, &out_file).unwrap();
-            println!("Result: {:?}", pak);
+            info!("OK");
         },
         "extract" => {
             let in_file = &args[2];
             let extract_path = &args[3];
-            println!("Extracting PAK file {} to {}", in_file, extract_path);
             let mut pak = PakFile::read(in_file).unwrap();
             pak.extract_to(extract_path).unwrap();
+            info!("OK");
         },
         "convert_lmp" => {
             let in_file = &args[2];
             let out_file = &args[3];
             let palette_file = &args[4];
-            println!("Converting {} to {} with palette file {}!", in_file, out_file, palette_file);
             let mut reader = io::BufReader::new(File::open(in_file).unwrap());
             let lmp = LmpImage::read(&mut reader).unwrap();
             let mut reader = io::BufReader::new(File::open(palette_file).unwrap());
             let palette = Palette::read(&mut reader).unwrap();
             
             lmp.save_as(out_file, palette).unwrap();
+            info!("OK");
         },
         "to_lmp" => {
             let in_file = &args[2];
@@ -59,6 +62,7 @@ fn main() {
             lmp_image.write(&mut writer).unwrap();
             let mut writer = File::create(&palette_out).unwrap();
             palette.write(&mut writer).unwrap();
+            info!("OK");
         }
         x => panic!("Unknown subcommand {}", x)
     }
